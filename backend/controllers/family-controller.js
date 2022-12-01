acb6d729f6137f623d1fd63684600112a1595510
Aika C <aika.carriere@gmail.com>
2022-12-01 13:05:35 -0600

    family controller, family service refactor. Registration logic

--- backend/controllers/family-controller.js
+++ backend/controllers/family-controller.js
@@ -2,22 +2,34 @@ const Family = require("../models/Family");
 const familyService = require("../service/family-service");
 
 const familyRegistration = async (req, res) => {
-    try {
-        console.log("========", req.body);
-        const { familyName } = req.body;
-        const principleId = req.body.principleId;
-        console.log(familyName, principleId);
-        console.log("welcome to the backend of the family");
+    console.log("========", req.body);
+    const { familyName } = req.body;
+    const principleId = req.body.principleId;
+    console.log(familyName, principleId);
+    console.log("welcome to the backend of the family");
 
-        const f1 = await familyService.familyRegistration(
+    try {
+        const isFamilyDuplicate = await familyService.isDuplicate(
             familyName,
             principleId
         );
-        console.log("f1:", f1);
-        res.status(201).send(f1);
+
+        if (!isFamilyDuplicate) {
+            const familyData = await familyService.familyRegistration(
+                familyName,
+                principleId
+            );
+            return res.status(201).json(familyData);
+        } else {
+            return res.status(409).json({
+                message: `the family with ${familyName} name already exists`,
+                //have to think what message to give to a user. Family name already exists under this Principle?  
+            });
+        }
     } catch (e) {
-        console.log(e);
-        res.status(409).send(e.message);
+        res.status(500).json({
+            message: `something went wrong`,
+        });
     }
 };
 
--- backend/service/family-service.js
+++ backend/service/family-service.js
@@ -2,21 +2,26 @@ const Family = require("../models/Family");
 
 const familyRegistration = async (familyName, principleId) => {
     // if cannot find the same email in the system then create the new user
-    const checkDuplicate = await Family.findOne({
-        familyName,
-        principle: principleId,
-    }).exec();
-    if (checkDuplicate) {
-        console.log("backend duplicate ");
-        throw new Error(`user with ${familyName} family name already exists`);
-    } else {
+  
         // 1) if there is such familyname, but no that person name , check if that person is invitated, if so add
         // 2) if there is no such family name, then create a new family with that principle
         const family = new Family({ familyName, principle: principleId });
         await family.save();
         console.log("family is", family);
-        return { principleId, familyId: family._id };
-    }
+        return {
+            familyName: family.familyName,
+            principleId,
+            familyId: family._id,
+        };
 };
 
-module.exports = { familyRegistration };
+const isDuplicate = async (familyName, principleId) => {
+    const checkDuplicate = await Family.findOne({
+        familyName,
+        principle: principleId,
+    });
+
+    return checkDuplicate ? true : false;
+};
+
+module.exports = { familyRegistration, isDuplicate };
