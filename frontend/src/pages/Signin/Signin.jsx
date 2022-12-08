928c06c3a5ed25f1585f0b7972a1a2907562b69f
eliza <twew02@gmail.com>
2022-12-08 13:30:03 -0500

    use formPasswordInput component, delete gitignore from backend

--- frontend/src/pages/Signin/Signin.jsx
+++ frontend/src/pages/Signin/Signin.jsx
@@ -1,6 +1,6 @@
 import "./Signin.css";
 import { useState, useEffect } from "react";
-import {Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
 
 import { login } from "../../api";
 import FatherSonBlock from "../../components/FatherSonBlock";
@@ -9,9 +9,11 @@ import Header from "../../components/Header/Header";
 import MessageBar from "../Register/MessageBar";
 import SocialButtonsGroup from "../../components/SocialButtonsGroup";
 import TextLink from "../../components/TextLink";
+import FormPasswordInput from "../../components/form/FormPasswordInput";
 
-
-const HeaderLink = <TextLink title="Not a member?" to="/register" linkTitle="Sign up" />;
+const HeaderLink = (
+  <TextLink title="Not a member?" to="/register" linkTitle="Sign up" />
+);
 
 export default function Signin() {
   const [email, setEmail] = useState("");
@@ -20,83 +22,74 @@ export default function Signin() {
   const [success, setSuccess] = useState(true);
 
   useEffect(() => {
-    setSuccess(true)
-  },[email, password])
-
-  console.log("email is ", email);
-  console.log("password is ", password);
-
+    setSuccess(true);
+  }, [email, password]);
 
   async function handleLogin(e) {
     e.preventDefault();
 
-    console.log("email is ", email);
-    console.log("password is ", password);
-    login(email, password).then((res)=> {
-      console.log(res.data)
-    if (res.data.message){
-      setSuccess(false)
-      setErrMsg('Your email address or password is incorrect. Please try again, or click "Forgot your password"')
-    } else {
-      setSuccess(true)
-      window.location.href = "/dashboard"
-    }
-    })
-  
+    login(email, password).then((res) => {
+      
+      if (res.data.message) {
+        setSuccess(false);
+        setErrMsg(
+          `Your email address or password is incorrect. Please try again, or click "Forgot your password"`
+        );
+      } else {
+        setSuccess(true);
+        window.location.href = "/dashboard";
+      }
+    });
   }
-  
+
   return (
     <>
-      <Header 
-        widget={HeaderLink}
-      />
-      <Container className="content-layout py-4" >
+      <Header widget={HeaderLink} />
+      <Container className="content-layout py-4">
         <FatherSonBlock>
-        
           <Form className="py-4" onSubmit={handleLogin}>
             <h1 className="login-title">Log in Kids First</h1>
-            
 
-            {!success &&
-            <MessageBar variant="error">
-            {errMsg}
-            </MessageBar>
-            }
-            <FormEmailInput 
-            type="email"
-                placeholder="example@email.com"
-                name="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                required
-                />
+            {!success && <MessageBar variant="error">{errMsg}</MessageBar>}
+            <FormEmailInput
+              type="email"
+              placeholder="example@email.com"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label className="password-label">Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                name="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              /> 
-               <div className="checkbox mb-3">
-                <a className=" btn forget-password" href="/Forget">
-                  Forgot your password?
-                </a>
-              </div>
-            </Form.Group>
-            <Button className="primary-btn w-100 my-5" type="submit" size="lg" variant="light">
-                Log In
-              </Button>
+            <FormPasswordInput
+              type="password"
+              placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+
             
+            <div className="checkbox mb-3">
+              <a className=" btn forget-password" href="/Forget">
+                Forgot your password?
+              </a>
+            </div>
+            
+            <Button
+              className="primary-btn w-100 my-5"
+              type="submit"
+              size="lg"
+              variant="light"
+            >
+              Log In
+            </Button>
 
             <div className="or-login-with">Or Log in with</div>
             <SocialButtonsGroup />
           </Form>
         </FatherSonBlock>
-        </Container>
+      </Container>
     </>
   );
-
 }
