4418cda892409f7d304fc0762ef510e26a347571
nadezdatsygankova <nadiapritchina@gamil.com>
2022-12-08 19:04:23 -0500

    delete firebase file

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,12 +3,6 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-//firebase
-import { authentication } from '../config/firebase-config';
-import { signInWithPopup, FacebookAuthProvider } from 'firebase/auth';
-
-
-
 import { register } from '../api';
 
 export default function Register() {
@@ -18,34 +12,6 @@ export default function Register() {
 
   console.log('email is ', email);
   console.log('password is ', password);
-
-//for facebook sign in
-const signInWithFacebook = () => {
-  const provider = new FacebookAuthProvider();
-
-  signInWithPopup(authentication,provider)
-  .then((result)=>{
-    console.log('I am registered using Facebook')
-    console.log(result.user)
-    const credential = FacebookAuthProvider.credentialFromResult(result);
-    const accessToken = credential.accessToken;
-    console.log(accessToken)
-    console.log(result.user.displayName)
-    console.log(result.user.email)
-    //we need to save this user in our database and redirect to family
-
-  })
-  .catch((error)=>{
-    console.log(error)
-    // Handle Errors here.
-  const errorCode = error.code;
-  const errorMessage = error.message;
-  // The email of the user's account used.
-  const email = error.customData.email;
-  // The AuthCredential type that was used.
-  const credential = FacebookAuthProvider.credentialFromError(error);
-  })
-};
 
   function handleRegister(e) {
     e.preventDefault();
@@ -97,7 +63,6 @@ const signInWithFacebook = () => {
           Register
         </Button>
       </Form>
-      <button onClick={signInWithFacebook}> Sign up with FB </button>
     </div>
   );
 }
