0ec472b43356891dc35721f8567781c5109d8042
Aika C <aika.carriere@gmail.com>
2022-12-15 15:40:08 -0600

    Frontend. Add a page that notifies a user about verifying email when they click an activation link
    -when a user click a link, account/email gets activated and user gets notified
    -have to think about design when data is not fetch yet (ex: status 'loading') and when the email verification token is corrupt or expired.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,31 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Register from "./pages/Register";
 import Family from "./pages/Family";
 import Member from "./pages/Member";
 import Dashboard from "./pages/Dashboard";
+import Activate from "./pages/Activate";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/family" element={<Family />} />
-          <Route path="/member" element={<Member />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/signin" element={<Signin />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route
+                        path="/activate/:email/:emailVerificationToken"
+                        element={<Activate />}
+                    />
+                    <Route path="/family" element={<Family />} />
+                    <Route path="/member" element={<Member />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
