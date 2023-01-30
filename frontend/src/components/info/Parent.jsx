0c3462de009403c54710435a0318632dd1219cc9
Zahra947 <85116470+Zahra947@users.noreply.github.com>
2023-01-30 19:08:35 +0330

    2 files modified, 3 added, 3 deleted

---
+++ frontend/src/components/info/Parent.jsx
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { parentPanel, panelStage } from '../pages/Panels/rootSlice.js';
+import { Container, Form, Button } from 'react-bootstrap';
+
+export default function Parent({
+  pageTitle,
+  submitButtonText,
+  previousButton,
+}) {
+  // redux
+  const dispatch = useDispatch();
+
+  // get Redux store values for Parent
+  const currentStage = useSelector((state) => state.panelStage); // for previous button
+  const parentPanelFirstName = useSelector(
+    (state) => state.parentPanel.firstName
+  );
+  const parentPanelLastName = useSelector(
+    (state) => state.parentPanel.lastName
+  );
+
+  // form values initial state
+  const [parentData, setParentData] = useState({
+    firstName: parentPanelFirstName || '',
+    lastName: parentPanelLastName || '',
+  });
+
+  // form values onchange
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setParentData({
+      ...parentData,
+      [name]: value,
+    });
+  };
+
+  // form validation checks
+  const [errors, setErrors] = useState({});
+  const validate = (parentData) => {
+    let formErrors = {}; // set form errors to none at start
+
+    // first name
+    if (!parentData.firstName) {
+      formErrors.firstName = 'First Name required';
+    }
+
+    // last name
+    if (!parentData.lastName) {
+      formErrors.lastName = 'Last Name required';
+    }
+
+    return formErrors;
+  };
+
+  const [isSubmitted, setIsSubmitted] = useState(false); // state for sent status
+  // onsubmit
+  const handleSubmit = (e) => {
+    e.preventDefault(); // stop form submission
+    setErrors(validate(parentData)); // check errors
+    setIsSubmitted(true); // update submit status
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitted) {
+      // check if any form errors
+
+      // update Redux Slice
+      dispatch(
+        panelStage(2) // update formStage
+      );
+      dispatch(
+        parentPanel({
+          // update parentPanel
+          firstName: parentData.firstName,
+          lastName: parentData.lastName,
+        })
+      );
+    }
+  }, [parentData, isSubmitted, dispatch, errors]);
+  // console.log(errors, parentData)
+
+  return (
+    <>
+      <h2>{pageTitle || 'Signup'}</h2>
+
+      <form
+        name="parent-panel"
+        id="parent-panel"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <p>
+          <label htmlFor="first-name">
+            First Name<span className="required-asterix">*</span>
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            autoComplete="First Name"
+            aria-label="first name"
+            aria-required="true"
+            placeholder="First name"
+            value={parentData.firstName}
+            onChange={handleChange}
+          />
+        </p>
+        {errors.name && <span className="error-message">{errors.name}</span>}
+
+        <p>
+          <label htmlFor="last-name">Last Name</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastLame"
+            autoComplete="Last Name"
+            aria-label="last anme"
+            aria-required="false"
+            placeholder="Last name"
+            value={parentData.lastName}
+            onChange={handleChange}
+          />
+        </p>
+
+        <p className="disclaimer-text">
+          <span className="required-asterix">*</span> required fields
+        </p>
+
+        <div className="btn-array">
+          {previousButton && (
+            <p>
+              <input
+                type="submit"
+                value={`Back`}
+                onClick={() => dispatch(panelStage(currentStage - 1))}
+              />
+            </p>
+          )}
+          <p>
+            <input type="submit" value={submitButtonText || 'Submit'} />
+          </p>
+        </div>
+      </form>
+    </>
+  );
+}
--- frontend/src/pages/Panels/Panels.jsx
+++ frontend/src/pages/Panels/Panels.jsx
@@ -1,12 +1,84 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import LazyLoad from 'react-lazyload';
+import Parent from '../components/info/Parent.jsx';
+import Co_Parent from '../components/info/Co_Parent.jsx';
+import Kids from '../components/info/Kids.jsx';
 
 export function Panels() {
-  retur(
-    <Router>
-      <Route exact path="/" component={My_Info}></Route>
-      <Route exact path="/Co_Parent" component={Co_Parent}></Route>
-      <Route exact path="/Kids" component={Kids}></Route>
-    </Router>
+  const panelStage = useSelector((state) => state.PanelSatge);
+  return (
+    <>
+      <section>
+        <LazyLoad once>
+          <div className="progressbar">
+            <div
+              className={
+                panelStage === 1
+                  ? 'progress-step progress-step-active'
+                  : 'progress-step'
+              }
+              data-title="Parent"
+            ></div>
+            <div
+              className={
+                panelStage === 2
+                  ? 'progress-step progress-step-active'
+                  : 'progress-step'
+              }
+              data-title="Co_Parent"
+            ></div>
+            <div
+              className={
+                panelStage === 3
+                  ? 'progress-step progress-step-active'
+                  : 'progress-step'
+              }
+              data-title="Kids"
+            ></div>
+          </div>
+        </LazyLoad>
+        <div className="page-wrapper">
+          {panelStage === 1 && (
+            // parent Page
+            <LazyLoad once>
+              <div className="wrap">
+                <Parent
+                  pageTitle={'Parent Info:'} // Panel title
+                  submitButtonText={'Next'} // submit next button display text
+                  previousButton={false} // show/hide previous button
+                />
+              </div>
+            </LazyLoad>
+          )}
+
+          {panelStage === 2 && (
+            // Co-Parent Page
+            <LazyLoad once>
+              <div className="wrap">
+                <Co_Parent
+                  pageTitle={'Co_Parent Info:'} // Panel title
+                  submitButtonText={'Next'} // submit next button display text
+                  previousButton={true} // show/hide previous button
+                />
+              </div>
+            </LazyLoad>
+          )}
+
+          {panelStage === 3 && (
+            // Kids Page
+            <LazyLoad once>
+              <div className="wrap">
+                <Kids
+                  pageTitle={'Kids Info:'} //  Panel title
+                  submitButtonText={'Submit'} // submit next button display text
+                  previousButton={true} // show/hide previous button
+                />
+              </div>
+            </LazyLoad>
+          )}
+        </div>
+      </section>
+    </>
   );
 }
