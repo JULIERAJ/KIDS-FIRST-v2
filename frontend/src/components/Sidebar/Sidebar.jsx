9020e21b057fbf8897b42bcb7963055c9e5dacc4
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-02-06 22:28:27 -0500

    fix height and add transition

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './Sidebar.module.css';
+
+import { SidebarData } from './SidebarData';
+
+import kidsFirstLogo from '../../../src/img/kids_first_logo_beta.png';
 
 const Sidebar = () => {
-  return <div>
-    HELLO SIDEBAR
-  </div>;
+  const sideBarIcon = SidebarData.map((icon, key) => {
+
+    return (
+      <nav className={`${styles.sidebarMenuItem}`} key={key}>
+        <Link to={icon.link} className={styles.sidebarLink}>
+          <img src={icon.icon} alt={icon.alt} />
+          <span>{icon.title}</span>
+        </Link>
+      </nav>
+    );
+  });
+  return (
+    <div className={styles.sidebar}>
+      <header className={styles.sidebarHeader}>
+        <img src={kidsFirstLogo} alt='mainLogo' />
+      </header>
+      <div className={styles.sidebarMenu}>{sideBarIcon}</div>
+    </div>
+  );
 };
 
 export default Sidebar;
