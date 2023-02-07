d5367a34579acb319d4eb5c109787182c238ceef
veranika-karpava <veranika.karpava@gmail.com>
2023-02-06 21:52:19 -0800

    Create data for list of cards

---
+++ frontend/src/data/features.data.jsx
@@ -0,0 +1,22 @@
+export const cardContent = [{
+  image: require('../media/features/calendar.png'),
+  title: 'Calendar',
+  // eslint-disable-next-line max-len
+  content: 'The calendar was built as an interface that allows users to easily to add events, view events from both parents and be able to communicate on any conflicts for events. The calendar was successfully designed to be a simple interface where users can easily distinguish how to create events, update events and delete them too.',
+  color: '#F7F5EC'
+},
+{
+  image: require('../media/features/child_info.png'),
+  title: 'Child Info',
+  // eslint-disable-next-line max-len
+  content: 'The child info repository is a feature that contains important information for all of the children. There are people the parents need to contact and places to potentially take their kids and this is one way for them to just keep the information there without having to ask each other for it.',
+  color: '#FFFFFF'
+},
+{
+  image: require('../media/features/law_popup.png'),
+  title: 'Law Pop-up',
+  // eslint-disable-next-line max-len
+  content: 'The law popups are a feature that can successfully analyze what has been inputted onto a page and send back facts pertaining to child custody law or just general facts about a child’s well-being. This feature was a specific request from one of the project supervisors, Dr. Bromwich, to help differentiate the product from the rest of the field.',
+  color: '#F7F5EC'
+}
+];
