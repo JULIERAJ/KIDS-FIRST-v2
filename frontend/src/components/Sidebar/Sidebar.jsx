c041d8409d4528b426ec1c609803f9ec91bcfee8
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-02-07 22:49:09 -0500

    change it to bootstrap and added prototypes

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,7 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { Image, Container, ListGroup } from 'react-bootstrap';
+
 import { Link } from 'react-router-dom';
 
 import styles from './Sidebar.module.css';
@@ -7,26 +10,43 @@ import { SidebarData } from './SidebarData';
 
 import kidsFirstLogo from '../../../src/img/kids_first_logo_beta.png';
 
-const Sidebar = () => {
-  const sideBarIcon = SidebarData.map((icon, key) => {
-
-    return (
-      <nav className={`${styles.sidebarMenuItem}`} key={key}>
-        <Link to={icon.link} className={styles.sidebarLink}>
-          <img src={icon.icon} alt={icon.alt} />
-          <span>{icon.title}</span>
-        </Link>
-      </nav>
-    );
-  });
+const SidebarItemsCard = ({ title, icon, path, alt }) => {
   return (
-    <div className={styles.sidebar}>
-      <header className={styles.sidebarHeader}>
-        <img src={kidsFirstLogo} alt='mainLogo' />
-      </header>
-      <div className={styles.sidebarMenu}>{sideBarIcon}</div>
-    </div>
+    <ListGroup as='ul' className={styles.sidebarMenu}>
+      <ListGroup.Item as='li' className={styles.sidebarMenuItem}>
+        <Image
+          as='img'
+          src={icon}
+          alt={alt}
+          style={{ width: '20.63px', height: '19.25px' }}
+        />
+        <Link as='a' to={path} className={styles.sidebarLink}>
+          {title}
+        </Link>
+      </ListGroup.Item>
+    </ListGroup>
   );
+};
+
+const Sidebar = () => {
+  return (
+    <Container as='div' className={`${styles.sidebar} no-gutter`}>
+      <div className={styles.sidebarHeader}>
+        <Image src={kidsFirstLogo} alt='mainLogo' />
+      </div>
+
+      {SidebarData.map((item, key) => {
+        return <SidebarItemsCard key={key} {...item} />;
+      })}
+    </Container>
+  );
+};
+
+SidebarItemsCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
 };
 
 export default Sidebar;
