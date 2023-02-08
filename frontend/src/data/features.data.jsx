471f2accb763743cf0907089360d80d7c2ff9043
veranika-karpava <veranika.karpava@gmail.com>
2023-02-07 22:43:15 -0800

    Add data for more features section

--- frontend/src/data/features.data.jsx
+++ frontend/src/data/features.data.jsx
@@ -1,22 +1,49 @@
-export const cardContent = [{
-  image: require('../media/features/calendar.png'),
+export const cardMainFeature = [{
+  imageUrl: require('../media/features/calendar.png'),
   title: 'Calendar',
   // eslint-disable-next-line max-len
   content: 'The calendar was built as an interface that allows users to easily to add events, view events from both parents and be able to communicate on any conflicts for events. The calendar was successfully designed to be a simple interface where users can easily distinguish how to create events, update events and delete them too.',
   color: '#F7F5EC'
 },
 {
-  image: require('../media/features/child_info.png'),
+  imageUrl: require('../media/features/child_info.png'),
   title: 'Child Info',
   // eslint-disable-next-line max-len
   content: 'The child info repository is a feature that contains important information for all of the children. There are people the parents need to contact and places to potentially take their kids and this is one way for them to just keep the information there without having to ask each other for it.',
   color: '#FFFFFF'
 },
 {
-  image: require('../media/features/law_popup.png'),
+  imageUrl: require('../media/features/law_popup.png'),
   title: 'Law Pop-up',
   // eslint-disable-next-line max-len
   content: 'The law popups are a feature that can successfully analyze what has been inputted onto a page and send back facts pertaining to child custody law or just general facts about a child’s well-being. This feature was a specific request from one of the project supervisors, Dr. Bromwich, to help differentiate the product from the rest of the field.',
   color: '#F7F5EC'
 }
+];
+
+export const cardExtraFeature = [
+  {
+    imageUrl: require('../media/features/finance_tracker.png'),
+    title: 'Finance Tracker',
+    // eslint-disable-next-line max-len
+    content: 'The finance tracker is a feature that allows for parents to track any expenses they have for their children. This interface acts as a ledger of all the transactions made for each child.',
+  },
+  {
+    imageUrl: require('../media/features/to_do_list.png'),
+    title: 'To-Do List',
+    // eslint-disable-next-line max-len
+    content: 'The finance tracker is a feature that allows for parents to track any expenses they have for their children. This interface acts as a ledger of all the transactions made for each child.',
+  },
+  {
+    imageUrl: require('../media/features/multilingual_interface.png'),
+    title: 'Multilingual Interface',
+    // eslint-disable-next-line max-len
+    content: 'We were able to successfully develop the application as a fully multilingual interface that allows for seamless switching between French and English. This allows the product to be catered to more demographics in Ontario.',
+  },
+  {
+    imageUrl: require('../media/features/report_generator.png'),
+    title: 'Report Generator',
+    // eslint-disable-next-line max-len
+    content: 'The application has two locations where behaviour should be tracked; the calendar and the finance tracker. In the calendar itself, the creation of events and the chat interface both need to be tracked.',
+  }
 ];
