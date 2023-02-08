7dcec4504bc338066af29ffc906465eb1852f83e
veranika-karpava <veranika.karpava@gmail.com>
2023-02-07 22:46:46 -0800

    Built card feature for more features section

--- frontend/src/components/CardFeature/CardFeature.jsx
+++ frontend/src/components/CardFeature/CardFeature.jsx
@@ -6,23 +6,21 @@ import Card from 'react-bootstrap/Card';
 
 import styles from './CardFeature.module.css';
 
-const CardFeature = ({ image, title, content, bgcolor }) => {
+const CardFeature = ({ image, title, content, bgcolor, main }) => {
 
   //eslint-disable-next-line
 
   return (
     <Card
-      className={styles.card}
-      style={{ backgroundColor: bgcolor }}>
+      className={main ? styles.card : styles.card__more}
+      style={main && { backgroundColor: bgcolor }}>
       <Card.Img variant="top" src={image} style={{ width: '11.25rem' }} />
-      <Card.Body className={styles.card__container}>
-        <Card.Title className={styles.card__title}>
-          {title}
-        </Card.Title>
-        <Card.Text className={styles.card__text}>
-          {content}
-        </Card.Text>
-      </Card.Body>
+      <Card.Title as='h3' className={main ? styles.card__title : styles.card__title_more}>
+        {title}
+      </Card.Title>
+      <Card.Text className={main ? styles.card__text : styles.card__text_more}>
+        {content}
+      </Card.Text>
     </Card>
   );
 };
@@ -31,7 +29,8 @@ CardFeature.propTypes = {
   bgcolor: PropTypes.string,
   image: PropTypes.string,
   title: PropTypes.string,
-  content: PropTypes.string
+  content: PropTypes.string,
+  main: PropTypes.string,
 };
 
 export default CardFeature;
