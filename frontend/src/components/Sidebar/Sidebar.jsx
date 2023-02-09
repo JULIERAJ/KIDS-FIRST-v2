ef3950c88482937ceb9583c3eb7af5e9b6c1c779
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-02-09 17:37:24 -0500

    replace list to nav

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Image, Container, ListGroup } from 'react-bootstrap';
-
-import { Link } from 'react-router-dom';
+import { Image, Container, Col, Nav, Row, Tab } from 'react-bootstrap';
 
 import styles from './Sidebar.module.css';
 
@@ -12,49 +10,55 @@ import kidsFirstLogo from '../../../src/img/kids_first_logo_beta.png';
 
 const SidebarItemsCard = ({ title, icon, path, alt }) => {
   return (
-    <ListGroup as='ul' className={styles.sidebarMenu}>
-      <ListGroup.Item
-        onClick={() => {
-          window.location.pathname = path;
-        }}
-        as='li'
-        className={styles.sidebarMenuItem}
-        id={window.location.pathname === path ? 'active' : ''}>
-        <Image
-          as='img'
-          src={icon}
-          alt={alt}
-          style={{
-            width: '20.63px',
-            height: '19.25px',
-          }}
-        />
-        <Link as='a' to={path} className={styles.sidebarLink}>
-          {title}
-        </Link>
-      </ListGroup.Item>
-    </ListGroup>
+    <Tab.Container as='div'>
+      <Row className={styles.rowContainer}>
+        <Col className={styles.sidebarMenu}>
+          <Nav as='ul'>
+            <Nav.Item
+              as='li'
+              className={styles.sidebarMenuItem}
+              id={window.location.pathname === path ? 'active' : ''}>
+              <Image
+                as='img'
+                src={icon}
+                alt={alt}
+                style={{
+                  width: '20.63px',
+                  height: '19.25px',
+                }}
+              />
+              <Nav.Link
+                eventKey='link-1'
+                to={path}
+                className={styles.sidebarLink}>
+                {title}
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </Row>
+    </Tab.Container>
   );
 };
 
 const Sidebar = () => {
   return (
-    <Container as='div' className={`${styles.sidebar} no-gutter`}>
+    <Container as='aside' className={`${styles.sidebar} no-gutter`}>
       <Container as='div' className={styles.sidebarHeader}>
         <Image src={kidsFirstLogo} alt='mainLogo' />
       </Container>
-      {SidebarData.map((item, key) => {
-        return <SidebarItemsCard key={key} {...item} />;
-      })}
+      {SidebarData.map((item, key) => (
+        <SidebarItemsCard key={key} {...item} />
+      ))}
     </Container>
   );
 };
 
 SidebarItemsCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
-  path: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  icon: PropTypes.string,
+  path: PropTypes.string,
+  alt: PropTypes.string,
   onClick: PropTypes.func,
 };
 
