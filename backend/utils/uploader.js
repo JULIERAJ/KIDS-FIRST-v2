7a421056d45571f7cef415021e4b581d837b2af0
veranika-karpava <veranika.karpava@gmail.com>
2023-03-10 14:28:54 -0800

    Create configuration file for upload image ising multer package

---
+++ backend/utils/uploader.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+};
+
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 10 * 1024 * 1024,  // max size 1MB
+        files: 1, // 1 files
+    },
+    fileFilter: (_req, file, cb) => {
+        const isValid = !!MIME_TYPE_MAP[file.mimetype]; // !! - convert to boolean
+        let error = isValid ? null : new Error('Invalid mime type!');
+        cb(error, isValid);
+    }
+});
+
+module.exports = upload;
