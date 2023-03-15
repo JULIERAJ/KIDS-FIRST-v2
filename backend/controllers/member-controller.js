377fb5d5c61837b59fc4508975652a1e21a55f58
veranika-karpava <veranika.karpava@gmail.com>
2023-03-15 16:44:18 -0700

    Create contoller for uploading and updating member's image specified Id

--- backend/controllers/member-controller.js
+++ backend/controllers/member-controller.js
@@ -1,4 +1,6 @@
 const memberService = require('../service/member-service');
+const Member = require('../models/Member');
+const { uid } = require('uid');
 
 const memberRegistration = async (req, res) => {
   const { firstname, lastname, principleId, familyId } = req.body;
@@ -10,7 +12,7 @@ const memberRegistration = async (req, res) => {
       principleId,
       familyId
     );
-    
+
     if (!isMemberDuplicate) {
       const memberData = await memberService.memberRegistration(
         firstname,
@@ -29,4 +31,43 @@ const memberRegistration = async (req, res) => {
   }
 };
 
-module.exports = { memberRegistration };
+const getMemberById = async (req, res) => {
+  const memberId = req.params.mid;
+
+  const specifiedMember = await Member.findById(memberId);
+  if (!specifiedMember) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  return res.status(201).json(specifiedMember);
+}
+
+// create member's image of specified member's Id
+const memberUploadImage = async (req, res) => {
+  try {
+    const memberId = req.params.mid;
+    const image = req.file;
+
+    const memberData = await memberService.uploadMemeberImage(memberId, image);
+    res.status(201).json(memberData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something went wrong', error: error });
+  }
+}
+
+const memberUpdateImage = async (req, res) => {
+  try {
+    const memberId = req.params.mid;
+    const image = req.file;
+
+    const memberUpdatedData = await memberService.updateMemeberImage(memberId, image);
+    res.status(201).json(memberUpdatedData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something went wrong', error: error });
+  }
+}
+
+
+module.exports = { memberRegistration, getMemberById, memberUploadImage, memberUpdateImage };
