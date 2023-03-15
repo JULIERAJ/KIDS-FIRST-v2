6d6246ca72effece34fb45a874df873bbcc1eb0e
veranika-karpava <veranika.karpava@gmail.com>
2023-03-15 16:45:03 -0700

    Create service functions for uploading and updating image

--- backend/service/member-service.js
+++ backend/service/member-service.js
@@ -1,4 +1,5 @@
 const Member = require('../models/Member');
+const { uid } = require('uid');
 
 const memberRegistration = async (
   firstname,
@@ -26,4 +27,70 @@ const isDuplicate = async (firstname, lastname, principleId, familyId) => {
   return checkDuplicate ? true : false;
 };
 
-module.exports = { memberRegistration, isDuplicate };
+const uploadMemeberImage = async (memberId, image) => {
+  try {
+    const specifiedMember = await Member.findById(memberId);
+    if (!specifiedMember) {
+      return { success: false, message: 'Member not found' };
+    }
+
+    if (!image) {
+      return { success: false, message: 'No image uploaded' };
+    }
+    // generate a unique filename for the image
+    const { mimetype, buffer } = image;
+    const fileName = `${uid()}.${mimetype.split('/')[1]}`;
+
+    specifiedMember.avatar = {
+      name: fileName,
+      contentType: mimetype,
+      data: buffer
+    }
+
+    await specifiedMember.save();
+    return { success: true, message: 'Image uploaded successfully', specifiedMember };
+  } catch (error) {
+    console.error(error);
+    return { success: false, message: 'Something went wrong', error };
+  }
+}
+
+const updateMemeberImage = async (memberId, image) => {
+  try {
+    if (!image) {
+      return { success: false, message: 'No image uploaded' };
+    }
+
+    const { mimetype, buffer } = image;
+    const fileName = `${uid()}.${mimetype.split('/')[1]}`;
+
+    const updatedMember = await Member.findOneAndUpdate(
+      { _id: memberId },
+      {
+        avatar: {
+          data: buffer,
+          contentType: mimetype,
+          fileName: fileName
+        }
+      },
+      { new: true }
+    );
+
+    if (!updatedMember) {
+      return { success: false, message: 'Member not found' };
+    }
+
+    // delete previous image
+    const { data: prevImageData } = updatedMember.avatar || {};
+    if (prevImageData) {
+      await Member.updateOne({ _id: memberId }, { $unset: { avatar: 1 } });
+    }
+
+    return { success: true, message: 'Image updated successfully', updatedMember };
+  } catch (error) {
+    console.error(error);
+    return { success: false, message: 'Something went wrong', error };
+  }
+}
+
+module.exports = { memberRegistration, isDuplicate, uploadMemeberImage, updateMemeberImage };
