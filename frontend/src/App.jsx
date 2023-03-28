39199d9af27b2addf84a634d857860f2fae72125
veranika-karpava <veranika.karpava@gmail.com>
2023-03-28 12:47:36 -0700

    Add route to MemberPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Family from './pages/Family';
 import ForgetPassword from './pages/ForgetPassword';
 import Home from './pages/Home';
 import Member from './pages/Member';
+import ProfileMember from './pages/ProfileMember';
 import Register from './pages/Register';
 import ResetPassword from './pages/ResetPassword';
 import Signin from './pages/Signin';
@@ -14,21 +15,22 @@ const App = () => (
   <BrowserRouter>
     <Routes>
       <Route>
-        <Route path='/' element={<Home />} />
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/register' element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/register" element={<Register />} />
         <Route
-          path='/activate/:email/:emailVerificationToken'
+          path="/activate/:email/:emailVerificationToken"
           element={<Activate />}
         />
-        <Route path='/family' element={<Family />} />
-        <Route path='/member' element={<Member />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/forgetPassword' element={<ForgetPassword />} />
+        <Route path="/family" element={<Family />} />
+        <Route path="/member" element={<Member />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/forgetPassword" element={<ForgetPassword />} />
         <Route
-          path='/resetPassword/:email/:resetPasswordToken'
+          path="/resetPassword/:email/:resetPasswordToken"
           element={<ResetPassword />}
         />
+        <Route path="/profileMemeber" element={<ProfileMember />} />
       </Route>
     </Routes>
   </BrowserRouter>
