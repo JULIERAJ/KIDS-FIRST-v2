1ee7f1a461c3a928443fba1d5fd7449184c06324
vrushali <vrushhaldankar16@gmail.com>
2024-03-24 18:34:41 -0400

    changes did to dashboard header

--- frontend/src/components/DashboardHeader/DashboardHeader.jsx
+++ frontend/src/components/DashboardHeader/DashboardHeader.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-
 import { Container } from 'react-bootstrap';
-
-import { FaCircle } from 'react-icons/fa';
 
 import styles from './DashboardHeader.module.css';
 
@@ -10,16 +7,16 @@ const DashboardHeader = () => {
   return (
     <Container as='aside' className={`${styles.Dashboard}`}>
       <div className={`${styles.heading}`}>
-        <h2 className={styles.rightAlign}>Dashboard</h2>
-        <h2 className={styles.leftAlign}>
-            Hello Emma!
-          <div className={styles.circleContainer}>
-            <FaCircle className={styles.FaCircle} />
-            <span className={styles.circleText}>EC</span>
-          </div>
-        </h2>
+        <h2 className={`${styles.rightAlign}`}>Dashboard</h2>
+      </div>
+      <div className={styles.title}>
+        <h2 className={`${styles.leftAlign}`}>Hello Emma!</h2>
+        <div className={styles.circleContainer}>
+          <span className={styles.circleText}>EC</span>
+        </div>
       </div>
     </Container>
   );
 };
+
 export default DashboardHeader;
