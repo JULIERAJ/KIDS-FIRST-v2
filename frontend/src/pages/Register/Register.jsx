9a0cfb167540b114288d0b79918dfdab1c77290c
vrushali <vrushhaldankar16@gmail.com>
2024-04-22 10:35:14 -0400

    changes made in register form

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 import { useParams } from 'react-router-dom';
 
 import EmailVerify from './EmailVerify';
@@ -7,12 +10,11 @@ import styles from './Register.module.css';
 import RegisterForm from './RegisterForm';
 
 import { register } from '../../api';
-import FatherSonBlock from '../../components/FatherSonBlock';
+// import FatherSonBlock from '../../components/FatherSonBlock';
 import Header from '../../components/Header';
 import TextLink from '../../components/TextLink';
 
 const Register = () => {
-
   const params = useParams();
   let paramEmail = params.email;
   /* eslint-disable no-unused-vars */
@@ -42,22 +44,35 @@ const Register = () => {
 
   return (
     <>
-      <Header
-        widget={
-          <TextLink title="Already a member?" to="/signin" linkTitle="Log in" />
-        }
-      />
-
-      <Container className="content-layout py-4" >
-        <FatherSonBlock>
-          <h1 className={styles.registerTitle}>Sign up Kids First</h1>
-          {
-            activeComponent ?
-              //set props for RegisterForm component with errorMessage
-              <RegisterForm onSubmitData={registerUserHandler} paramEmail={paramEmail} errorMessage={errorMessage}/> :
-              <EmailVerify userData={userData}/>
+      <div>
+        <Header
+          widget={
+            <TextLink
+              title='Already a member?'
+              to='/signin'
+              linkTitle='Log in'
+            />
           }
-        </FatherSonBlock>
+        />
+      </div>
+
+      <Container className='content-layout py-4'>
+        <Row>
+          <Col className='d-flex justify-content-center align-items-center'>
+            <div>
+              <h1 className={styles.registerTitle}>Sign up Kids First</h1>
+              {activeComponent ? (
+                <RegisterForm
+                  onSubmitData={registerUserHandler}
+                  paramEmail={paramEmail}
+                  errorMessage={errorMessage}
+                />
+              ) : (
+                <EmailVerify userData={userData} />
+              )}
+            </div>
+          </Col>
+        </Row>
       </Container>
     </>
   );
