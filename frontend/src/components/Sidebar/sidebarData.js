e1bdc2d9a81e386af1566b4ff83d35b71f1c7eaa
Nadia Tsygankova <nadiapritchina@gmail.com>
2024-05-16 23:47:55 -0400

    new icons

--- frontend/src/components/Sidebar/sidebarData.js
+++ frontend/src/components/Sidebar/sidebarData.js
@@ -1,65 +1,135 @@
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import ChatIcon from '@mui/icons-material/Chat';
-import CollectionsIcon from '@mui/icons-material/Collections';
-import FaceIcon from '@mui/icons-material/Face';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import HomeIcon from '@mui/icons-material/Home';
-import SettingsIcon from '@mui/icons-material/Settings';
 
 export const SIDEBAR_DATA = [
   {
     title: 'Dashboard',
-    icon: <HomeIcon color="action" sx={{ fontSize: 32 }} />,
-    activeIcon: <HomeIcon sx={{ color: '#F08324', fontSize: 32 }} />,
-    hoverIcon:  <HomeIcon sx={{ fontSize: 32 }} />,
+    icon: <span className="material-symbols-rounded home "
+      style={{ 'font-size': '32px' }}>home</span>,
+    activeIcon:<span className="material-symbols-rounded home "
+      style={{ 'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
+        color: '#DB5C00'
+      }}
+    >home</span>,
+    hoverIcon:
+    <span className="material-symbols-rounded home "
+      style={{
+        'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
+      }}>home</span>,
     path: '/dashboard',
     alt: 'dashboard-icon',
   },
   {
     title: 'Calendar',
-    icon: <CalendarMonthIcon color="action" sx={{ fontSize: 32 }} />,
-    activeIcon: <CalendarMonthIcon sx={{ color: '#F08324', fontSize: 32 }}/>,
-    hoverIcon: <CalendarMonthIcon sx={{ fontSize: 32 }} />,
+    icon: <span className="material-symbols-rounded home "
+      style={{ 'font-size': '32px' }}>calendar_month</span>,
+    activeIcon:<span className="material-symbols-rounded home "
+      style={{ 'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
+        color: '#DB5C00'
+      }}
+    >calendar_month</span>,
+    hoverIcon:
+    <span className="material-symbols-rounded home "
+      style={{
+        'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
+      }}>calendar_month</span>,
     path: '/calendarview',
     alt: 'calendar-icon',
   },
   {
     title: 'Messages',
-    icon: <ChatIcon color="action" sx={{ fontSize: 32 }}/>,
-    activeIcon: <ChatIcon sx={{ color: '#F08324', fontSize: 32 }}/>,
-    hoverIcon: <ChatIcon sx={{ fontSize: 32 }} />,
+    icon: <span className="material-symbols-rounded "
+      style={{ 'font-size': '32px' }}>chat</span>,
+    activeIcon:<span className="material-symbols-rounded"
+      style={{ 'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
+        color: '#DB5C00'
+      }}
+    >chat</span>,
+    hoverIcon:
+    <span className="material-symbols-rounded"
+      style={{
+        'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
+      }}>chat</span>,
     path: '/messages',
     alt: 'messages-icon',
   },
   {
     title: 'Album',
-    icon: <CollectionsIcon color="action" sx={{ fontSize: 32 }}/>,
-    activeIcon: <CollectionsIcon sx={{ color: '#F08324', fontSize: 32 }}/>,
-    hoverIcon: <CollectionsIcon sx={{ fontSize: 32 }}/>,
+    icon: <span className="material-symbols-rounded"
+      style={{ 'font-size': '32px' }}>photo_library</span>,
+    activeIcon:<span className="material-symbols-rounded"
+      style={{ 'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
+        color: '#DB5C00'
+      }}
+    >photo_library</span>,
+    hoverIcon:
+    <span className="material-symbols-rounded"
+      style={{
+        'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
+      }}>photo_library</span>,
     path: '/album',
     alt: 'album-icon',
   },
   {
     title: 'Kids’ Info',
-    icon: <FaceIcon color="action" sx={{ fontSize: 32 }}/>,
-    activeIcon: <FaceIcon sx={{ color: '#F08324', fontSize: 32 }}/>,
-    hoverIcon: <FaceIcon sx={{ fontSize: 32 }} />,
+    icon: <span className="material-symbols-rounded"
+      style={{ 'font-size': '32px' }}>face</span>,
+    activeIcon:<span className="material-symbols-rounded"
+      style={{ 'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
+        color: '#DB5C00'
+      }}
+    >face</span>,
+    hoverIcon:
+    <span className="material-symbols-rounded"
+      style={{
+        'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
+      }}>face</span>,
     path: '/kids',
     alt: 'kids-icon',
   },
   {
     title: 'Settings',
-    icon: <SettingsIcon color="action" sx={{ fontSize: 32 }}/>,
-    activeIcon:<SettingsIcon sx={{ color: '#F08324', fontSize: 32 }}/>,
-    hoverIcon:<SettingsIcon sx={{ fontSize: 32 }} />,
+    icon: <span className="material-symbols-rounded"
+      style={{ 'font-size': '32px' }}>settings</span>,
+    activeIcon:<span className="material-symbols-rounded"
+      style={{ 'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
+        color: '#DB5C00'
+      }}
+    >settings</span>,
+    hoverIcon:
+    <span className="material-symbols-rounded"
+      style={{
+        'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
+      }}>settings</span>,
     path: '/settings',
     alt: 'settings-icon',
   },
   {
     title: 'Help',
-    icon: <HelpOutlineIcon color="action" sx={{ fontSize: 32 }}/>,
-    activeIcon: <HelpOutlineIcon sx={{ color: '#F08324', fontSize: 32 }}/>,
-    hoverIcon: <HelpOutlineIcon sx={{ fontSize: 32 }} />,
+    icon: <span className="material-symbols-rounded"
+      style={{ 'font-size': '32px' }}>help</span>,
+    activeIcon:<span className="material-symbols-rounded"
+      style={{ 'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40',
+        color: '#DB5C00'
+      }}
+    >help</span>,
+    hoverIcon:
+    <span className="material-symbols-rounded"
+      style={{
+        'font-size': '32px',
+        fontVariationSettings: '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 40'
+      }}>help</span>,
     path: '/help',
     alt: 'help-icon',
   },
